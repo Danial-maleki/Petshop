c0e943b95a05087083f48a550efcce4f97daca2d
Danial-maleki <danialamg64@gmail.com>
2025-09-09 13:17:52 +0330

    new change: into this update i will add logger to the program

--- PetShopV2/animal.h
+++ PetShopV2/animal.h
@@ -3,17 +3,25 @@
 
 #include <QString>
 #include <QDebug>
+#include <QObject>
+#include <QFile>
+#include <QDir>
+#include <QDateTime>
 class Animal
 {
 public:
     Animal(const QString &name, int price);
+    // Logger(QObject *parent = nullptr);
     virtual ~Animal();
-
+    virtual QString sound() const = 0;
+////////////////////////////////////////////////////
     QString getName() const;
     int getPrice() const;
-
-    virtual QString sound() const = 0;
-
+///////////////////////////////////////
+    static bool logging;
+    static QString filename;
+    static void attach();
+    static void handler(QtMsgType type, const QMessageLogContext &context, const QString &msg);
 private:
     QString m_name;
     int m_price;
